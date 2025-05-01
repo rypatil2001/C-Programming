36f05aa19620fd931de5b2e1dfab404e8d8682b3
Raj Patil <rypatil2001@gmail.com>
2025-05-02 00:48:06 +0530

    Add files via upload

---
+++ 1_Main_Function.c
@@ -0,0 +1,11 @@
+// First C program
+
+#include<stdio.h> // include standard input output (printf, scanf) header files.
+
+int main()  //entry point function, the compiler searches it first
+{
+    printf("Hello World !!");
+    printf("This is a Tribute to THE Dennis Ritchie...");
+
+    return 0; // Indicates successfull Execution to the compiler (0 - because return type of main function is of inter format)
+}
